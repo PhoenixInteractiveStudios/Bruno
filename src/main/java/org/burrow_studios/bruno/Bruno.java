af941080bf4d27b363362b84552ebdf4849d0c54
TurtleException <turtle.exception@eldritch.de>
2023-12-09 00:31:27 +0100

    Create config.properties & add JDA

--- src/main/java/org/burrow_studios/bruno/Bruno.java
+++ src/main/java/org/burrow_studios/bruno/Bruno.java
@@ -1,7 +1,52 @@
 package org.burrow_studios.bruno;
 
-public class Bruno {
-    Bruno() {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
+import org.burrow_studios.bruno.util.ResourceUtil;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+public class Bruno {
+    /** Directory iun which the JAR ist located. */
+    public static final File DIR;
+    static {
+        File f;
+        try {
+            f = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+        } catch (URISyntaxException e) {
+            System.out.println("Failed to declare directory");
+            throw new RuntimeException(e);
+        }
+        DIR = f;
+    }
+
+    private final JDABuilder jdaBuilder;
+    private JDA jda;
+
+    Bruno() throws InvalidTokenException, IllegalArgumentException {
+        jdaBuilder= JDABuilder.createDefault(null);
+    }
+
+    void run() throws IOException {
+        // Shutdown
+        if (jda != null && !jda.getStatus().equals(JDA.Status.SHUTDOWN))
+            jda.shutdown();
+
+        // Write default config
+        ResourceUtil.createDefault("config.properties");
+
+        // Import config
+        Properties config = new Properties();
+        config.load(new FileReader(new File(DIR, "config.properties")));
+
+        // Set bot token
+        jdaBuilder.setToken(config.getProperty("token"));
+
+        jda = jdaBuilder.build();
     }
 }
--- src/main/java/org/burrow_studios/bruno/util/ResourceUtil.java
+++ src/main/java/org/burrow_studios/bruno/util/ResourceUtil.java
@@ -1,10 +1,11 @@
 package org.burrow_studios.bruno.util;
 
+import org.burrow_studios.bruno.Bruno;
 import org.burrow_studios.bruno.Main;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 public class ResourceUtil {
@@ -28,6 +29,28 @@ public class ResourceUtil {
             System.out.println("\nCould not load version from resources.");
             e.printStackTrace();
             return null;
+        }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void createDefault(@NotNull String resource) throws IOException {
+        File file = new File(Bruno.DIR, resource);
+        if (file.exists()) return;
+
+        file.createNewFile();
+
+        try(
+                InputStream   inStream = Main.class.getResourceAsStream(resource);
+                OutputStream outStream = new FileOutputStream(file)
+        ) {
+            if (inStream == null)
+                throw new IllegalArgumentException("No such resource: " + resource);
+
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            while ((readBytes = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, readBytes);
+            }
         }
     }
 }
