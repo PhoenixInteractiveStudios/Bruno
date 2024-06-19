660acc946aea9d648defb1ababaf7ccb73a815db
TurtleException <turtle.exception@eldritch.de>
2024-06-19 23:55:01 +0200

    Implement text.json & TextProvider

--- src/main/java/org/burrow_studios/bruno/Bruno.java
+++ src/main/java/org/burrow_studios/bruno/Bruno.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.burrow_studios.bruno.listener.DashboardCleaner;
+import org.burrow_studios.bruno.text.TextProvider;
 import org.burrow_studios.bruno.util.ResourceTools;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -17,8 +18,10 @@ import java.io.IOException;
 public class Bruno {
     private static final Logger LOG = LoggerFactory.getLogger(Bruno.class);
 
-    private Config config;
+    private TextProvider textProvider;
     private JDA jda;
+
+    private Config config;
 
     public Bruno() { }
 
@@ -28,8 +31,14 @@ public class Bruno {
         LOG.debug("Creating default config file");
         ResourceTools.get(Main.class).createDefault(Main.DIR, "config.properties");
 
+        LOG.debug("Creating default text.json");
+        ResourceTools.get(Main.class).createDefault(Main.DIR, "text.json");
+
         LOG.debug("Reading config");
         this.config = Config.fromFile(new File(Main.DIR, "config.properties"));
+
+        LOG.info("Parsing text.json");
+        this.textProvider = new TextProvider(new File(Main.DIR, "text.json"));
 
 
         LOG.info("Initializing JDA");
@@ -67,6 +76,10 @@ public class Bruno {
         }
 
         LOG.info("OK bye");
+    }
+
+    public @NotNull TextProvider getTextProvider() {
+        return this.textProvider;
     }
 
     public @NotNull Config getConfig() {
